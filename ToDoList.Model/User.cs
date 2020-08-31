d8653ba70274abf90ed1755f4bbbf82f73fdb971
Tomasz Kumiega <tomekkumiega@interia.pl>
2020-08-31 14:17:23 +0200

    Add user model class

---
+++ ToDoList.Model/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoList.Model
+{
+    public class User
+    {
+        public string Name { get; private set; }
+        public int Karma { get; set; }
+
+        /// <summary>
+        /// Initializes new instance of <see cref="User"/> class
+        /// </summary>
+        /// <param name="name"></param>
+        public User(string name)
+        {
+            Name = name;
+        }
+    }
+}
