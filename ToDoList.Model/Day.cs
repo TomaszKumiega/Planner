9b96d1622c29539461986e7a70d15c87816e723c
Tomasz Kumiega <tomekkumiega@interia.pl>
2020-08-31 15:12:40 +0200

    Add day model class

---
+++ ToDoList.Model/Day.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoList.Model
+{
+    public class Day
+    {
+        public DateTime Date { get; }
+        public List<Event> Events { get; }
+
+        public Day(DateTime date)
+        {
+            Date = date;
+            Events = new List<Event>();
+        }
+
+        public void AddEvent(Event e)
+        {
+            Events.Add(e);
+        }
+
+        public void RemoveEvent(Event e)
+        {
+            Events.Remove(e);
+        }
+    }
+}
