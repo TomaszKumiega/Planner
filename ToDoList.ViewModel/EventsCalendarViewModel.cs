e7edde3f013c49a64cff515dde26365a7a769aaf
Tomasz Kumiega <tomekkumiega@interia.pl>
2020-08-31 14:30:00 +0200

    Add Events Calendar ViewModel

---
+++ ToDoList.ViewModel/EventsCalendarViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ToDoList.Model;
+
+namespace ToDoList.ViewModel
+{
+    public class EventsCalendarViewModel
+    {
+        public User User { get; private set; }
+        public List<Event> DisposableEvents { get; }
+        public List<Event> RepetetiveEvents { get; }
+        public DateTime CurrentlyDisplayedMonth { get; private set; }
+        public int CurrentlySelectedDay { get; private set; }
+
+        public EventsCalendarViewModel()
+        {
+            
+        }
+    }
+}
