b0ec4141d22180f83b05fa9f87b91e50b51e9647
Tomasz Kumiega <tomekkumiega@interia.pl>
2020-08-31 15:16:06 +0200

    Refactor properties of EventsCalendarViewModel

--- ToDoList.ViewModel/EventsCalendarViewModel.cs
+++ ToDoList.ViewModel/EventsCalendarViewModel.cs
@@ -8,10 +8,10 @@ namespace ToDoList.ViewModel
     public class EventsCalendarViewModel
     {
         public User User { get; private set; }
-        public List<Event> DisposableEvents { get; }
-        public List<Event> RepetetiveEvents { get; }
         public DateTime CurrentlyDisplayedMonth { get; private set; }
-        public int CurrentlySelectedDay { get; private set; }
+        public List<Day> DisplayedDays { get; private set; }
+        public List<Event> RepetetiveEvents { get; }
+        public DateTime CurrentlySelectedDay { get; private set; }
 
         public EventsCalendarViewModel()
         {
