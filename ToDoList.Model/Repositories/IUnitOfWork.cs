d19e6d43edf71a55f7ea778ac7439e14790039d6
Tomasz Kumiega <tomekkumiega@interia.pl>
2020-09-01 19:35:27 +0200

    Add unit of work

---
+++ ToDoList.Model/Repositories/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoList.Model.Repositories
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<User> UserRepository { get; }
+        IRepository<Day> DayRepository { get; }
+        IRepository<Event> EventRepository { get; }
+
+        int SaveChanges();
+
+    }
+}
