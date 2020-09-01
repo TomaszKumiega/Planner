0675b4a620862262571710b1c1a8583cf74993d8
Tomasz Kumiega <tomekkumiega@interia.pl>
2020-09-01 17:22:42 +0200

    Add IRepository interface

---
+++ ToDoList.Model/Repositories/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace ToDoList.Model.Repositories
+{
+    public interface IRepository<T>
+    {
+        IEnumerable<T> GetAll();
+        T GetById(Guid id);
+
+        void Add(T entity);
+        void AddRange(IEnumerable<T> entities);
+
+        void Update(T entity);
+
+        void Remove(Guid id);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+
+        IEnumerable<T> Find(Expression<Func<T, bool>> predicate);
+    }
+}
