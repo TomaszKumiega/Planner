4d3efc1bb99bfaf97130c3c156294a2a02137869
Tomasz Kumiega <tomekkumiega@interia.pl>
2020-09-03 00:33:23 +0200

    Finish LoadSchedule method

--- ToDoList.ViewModel/EventsCalendarViewModel.cs
+++ ToDoList.ViewModel/EventsCalendarViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using ToDoList.Model;
 using ToDoList.Model.Repositories;
 
@@ -11,7 +12,7 @@ namespace ToDoList.ViewModel
     {
         public User User { get; private set; }
         public DateTime CurrentlyDisplayedMonth { get; private set; }
-        public List<Day> DisplayedDays { get; private set; }
+        public Dictionary<DateTime, List<Event>> Schedule { get; private set; }
         public DateTime CurrentlySelectedDay { get; private set; }
 
         private readonly IUnitOfWork _unitOfWork;
@@ -42,24 +43,12 @@ namespace ToDoList.ViewModel
         
         public void CreateEvent(string name, EventType eventType, EventDifficulty eventDifficulty, DateTime dateTime)
         {
-            var days = _unitOfWork.DayRepository.Find(x => x.Date.Date == dateTime.Date).ToList();
-            var e = new Event(name, eventType,eventDifficulty, days[0]);
+            var e = new Event(name, eventType,eventDifficulty, dateTime);
 
-            if (days.Any())
-            {
+            _unitOfWork.EventRepository.Add(e);
+            _unitOfWork.SaveChanges();
 
-                days[0].Events.Add(e);
-                _unitOfWork.SaveChanges();
-            }
-            else
-            {
-                var day = new Day(dateTime);
-                day.Events.Add(e);
-                _unitOfWork.DayRepository.Add(day);
-                _unitOfWork.SaveChanges();
-            }
-
-            LoadDisplayedDays();
+            LoadScheduleAsync();
         }
 
 
@@ -70,12 +59,70 @@ namespace ToDoList.ViewModel
             _unitOfWork.EventRepository.Add(e);
             _unitOfWork.SaveChanges();
 
-            LoadDisplayedDays();
+            LoadScheduleAsync();
         }
 
-        private void LoadDisplayedDays()
+        private async Task LoadScheduleAsync()
         {
+            Schedule = new Dictionary<DateTime, List<Event>>();
+            
+            var days = GetCurrentlyDisplayedDays();
+            var repetetiveEvents = await Task.Run(() => _unitOfWork.EventRepository.Find(x => x.RepetitionPattern != null).ToList());
 
+            foreach (var t in days)
+            {
+                var listOfEvents = new List<Event>();
+
+                //Finds all repetetive events that are going to happen on the day t
+                foreach (var k in repetetiveEvents)
+                {
+                    if (k.IsDateTimeMatchingRepetitionPattern(t)) listOfEvents.Add(k);
+                }
+            
+                // Finds all disposable events that are going to happen on the day t
+                listOfEvents.AddRange( await Task.Run(() => _unitOfWork.EventRepository.Find(x => x.DateTime.Value.Date == t.Date).ToList()));
+
+                Schedule.Add(t, listOfEvents);
+            }
+        }
+
+        private List<DateTime> GetCurrentlyDisplayedDays()
+        {
+            var listOfDays = new List<DateTime>();
+            var date = new DateTime(CurrentlyDisplayedMonth.Year, CurrentlyDisplayedMonth.Month, 1);
+
+            // Generate days from previous months to fill a gap at the begining of the calendar
+            int numberOfDaysFromPreviousMonth;
+
+            // Sunday is 0
+            if ((int)date.DayOfWeek == 0) numberOfDaysFromPreviousMonth = 6;
+            else numberOfDaysFromPreviousMonth = (int)date.DayOfWeek - 1;
+
+            date.AddDays(-numberOfDaysFromPreviousMonth);
+
+            for (int i=0;i<numberOfDaysFromPreviousMonth;i++)
+            {
+                listOfDays.Add(date);
+                date.AddDays(1);
+            }
+
+            // Generate list of days from currently displayed month
+            listOfDays.AddRange(Enumerable.Range(1, DateTime.DaysInMonth(date.Year, date.Month))
+                .Select(day => new DateTime(date.Year, date.Month, day))
+                .ToList());
+
+            // Generate days from next month to fill a gap at the end of the calendar
+            date.AddMonths(1);
+
+            var numberOfDaysFromNextMonth = 35 - listOfDays.Count;
+
+            for (int i=0; i<numberOfDaysFromNextMonth;i++)
+            {
+                listOfDays.Add(date);
+                date.AddDays(1);
+            }
+            
+            return listOfDays;
         }
 
         //TODO:
