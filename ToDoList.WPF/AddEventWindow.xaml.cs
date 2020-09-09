edb6ac8c2166b57f922a29bd8cf7da0219f84546
Tomasz Kumiega <tomekkumiega@interia.pl>
2020-09-09 04:09:15 +0200

    Add Event adding window

---
+++ ToDoList.WPF/AddEventWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ToDoList.WPF
+{
+    /// <summary>
+    /// Interaction logic for AddEventWindow.xaml
+    /// </summary>
+    public partial class AddEventWindow : Window
+    {
+        public AddEventWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
