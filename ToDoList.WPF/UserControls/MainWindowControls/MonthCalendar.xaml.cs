c675c49df97ce24630ac3966c8d9805514632d1d
Tomasz Kumiega <tomekkumiega@interia.pl>
2020-09-12 07:44:53 +0200

    Add days loading

--- ToDoList.WPF/UserControls/MainWindowControls/MonthCalendar.xaml.cs
+++ ToDoList.WPF/UserControls/MainWindowControls/MonthCalendar.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,6 +12,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using ToDoList.ViewModel.ObserverPattern;
+using ToDoList.ViewModel.ViewModels;
 
 namespace ToDoList.WPF
 {
@@ -19,10 +21,74 @@ namespace ToDoList.WPF
     /// </summary>
     public partial class MonthCalendar : UserControl, IObserver
     {
-        public MonthCalendar()
+        public MonthCalendar(IEventsCalendarViewModel viewModel)
         {
             InitializeComponent();
             InitializeStrings();
+            DataContext = viewModel;
+            viewModel.AddObserver(this);
+            InitializeDayTextBlocks();
+        }
+
+        private void LoadEvents()
+        {
+            var viewModel = (DataContext as IEventsCalendarViewModel);
+            
+            
+        }
+
+        private void InitializeDayTextBlocks()
+        {
+            var viewModel = (DataContext as IEventsCalendarViewModel);
+            
+            for (int i = 0; i < 5; i++)
+            {
+                var tb = new TextBlock();
+                int t = 0;
+                Grid.SetColumn(tb, 0);
+                Grid.SetRow(tb, i+1);
+
+                tb.FontSize = 18;
+                tb.Foreground = Brushes.WhiteSmoke;
+                tb.Text = viewModel.Schedule.ElementAt(i*7).Key.Day.ToString();
+                tb.Name = "Day" + ((i * 7) + (t + 1) - 1).ToString() + "TextBlock";
+                RootGrid.Children.Add(tb);
+
+                for(;t<7;t++)
+                {
+                    var textBlock = new TextBlock();
+
+                    Grid.SetColumn(textBlock, t);
+                    Grid.SetRow(textBlock,i+1);
+
+                    textBlock.FontSize = 18;
+                    textBlock.Foreground = Brushes.WhiteSmoke;
+                    textBlock.Text = viewModel.Schedule.ElementAt((i * 7) + (t + 1) - 1).Key.Day.ToString();
+                    textBlock.Name = "Day" + ((i * 7) + (t + 1) - 1).ToString() + "TextBlock";
+                    RootGrid.Children.Add(textBlock);
+                }
+            }
+        }
+       
+        private void LoadDays()
+        {
+            foreach (var t in RootGrid.Children)
+            {
+                var tb = t as TextBlock;
+                if (tb != null)
+                {
+                    if (tb.Name.StartsWith("Day"))
+                    {
+                        for (int i = 0; i < 35; i++)
+                        {
+                            if (tb.Name.StartsWith("Day" + i.ToString()))
+                            {
+                                tb.Text = (DataContext as IEventsCalendarViewModel).Schedule.ElementAt(i).Key.Day.ToString();
+                            }
+                        }
+                    }
+                }
+            }
         }
 
         private void InitializeStrings()
@@ -38,7 +104,8 @@ namespace ToDoList.WPF
 
         public void Update()
         {
-            throw new NotImplementedException();
+            LoadEvents();
+            LoadDays();
         }
 
     }
