5beb8107dabf5aa7d118f41bcacbcec54ba7b44d
Tomasz Kumiega <tomekkumiega@interia.pl>
2020-09-12 07:00:06 +0200

    Add user control factory

---
+++ ToDoList.WPF/UserControls/IUserControlFactory.cs
@@ -0,0 +1,11 @@
+﻿using ToDoList.Model;
+using ToDoList.ViewModel.ViewModels;
+
+namespace ToDoList.WPF.UserControls
+{
+    public interface IUserControlFactory
+    {
+        EventUserControl GetEventUserControl(IEventsCalendarViewModel viewModel, Event @event);
+        MonthCalendar GetMonthCalendar(IEventsCalendarViewModel viewModel);
+    }
+}
---
+++ ToDoList.WPF/UserControls/UserControlFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ToDoList.Model;
+using ToDoList.ViewModel.ViewModels;
+
+namespace ToDoList.WPF.UserControls
+{
+    public class UserControlFactory : IUserControlFactory
+    {
+        public MonthCalendar GetMonthCalendar(IEventsCalendarViewModel viewModel)
+        {
+            return new MonthCalendar(viewModel);
+        }
+
+        public EventUserControl GetEventUserControl(IEventsCalendarViewModel viewModel, Event @event)
+        {
+            return new EventUserControl(viewModel, @event);
+        }
+    }
+}
