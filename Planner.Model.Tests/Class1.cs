fc4c70445e22cffb9c22a28423900b02b5f1fe7d
Tomasz Kumiega <tomekkumiega@interia.pl>
2020-09-13 10:27:59 +0200

    Add DaysCompleted test

--- Planner.Model.Tests/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Planner.Model.Tests
-{
-    public class Class1
-    {
-    }
-}
---
+++ Planner.Model.Tests/EventTests.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Schema;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Planner.Model
+{
+    public class EventTests
+    {
+        [Fact]
+        public void DaysCompleted_ShouldBeAbleToRetrieveAddedValue()
+        {
+            var @event = new Event();
+            var daysList = new List<DateTime>();
+            var dateTime = DateTime.Now;
+            daysList.Add(dateTime);
+
+            @event.DaysCompleted = daysList;
+
+            Assert.True(@event.DaysCompleted[0].Equals(dateTime));
+        }
+    }
+}
