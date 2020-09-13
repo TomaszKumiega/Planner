2db74662eee8d28b112dd68a53c1507cbbf6bf08
Tomasz Kumiega <tomekkumiega@interia.pl>
2020-09-13 09:21:05 +0200

    Add logger to EventRepository class

--- Planner.Model/Repositories/EventRepository.cs
+++ Planner.Model/Repositories/EventRepository.cs
@@ -8,11 +8,14 @@ namespace Planner.Model.Repositories
 {
     public class EventRepository : IRepository<Event>
     {
+        private static readonly NLog.Logger _logger = NLog.LogManager.GetCurrentClassLogger();
+
         private readonly ScheduleDbContext _context;
 
         public EventRepository(ScheduleDbContext context)
         {
             _context = context;
+            _logger.Debug("Event repository created.");
         }
 
         public void Add(Event entity)
@@ -21,6 +24,8 @@ namespace Planner.Model.Repositories
             if (entity.Id != null) throw new ArgumentException("Entity doesnt have Id");
 
             _context.Events.Add(entity);
+
+            _logger.Info("Entity: " + entity.Id.ToString() + "was added to the database");
         }
 
         public void AddRange(IEnumerable<Event> entities)
@@ -30,6 +35,8 @@ namespace Planner.Model.Repositories
             if (entities.Where(x => x.Id == null).ToList().Any()) throw new ArgumentException("Entity doesnt have Id");
 
             _context.Events.AddRange(entities);
+
+            foreach (var t in entities) _logger.Info("Entity: " + t.Id.ToString() + "was added to the database");
         }
 
         public IEnumerable<Event> Find(Expression<Func<Event, bool>> predicate)
@@ -57,6 +64,7 @@ namespace Planner.Model.Repositories
             if (events.Count != 0)
             {
                 _context.Events.Remove(events[0]);
+                _logger.Info("Event: " + id.ToString() + " was removed from database");
             }
         }
 
@@ -65,6 +73,7 @@ namespace Planner.Model.Repositories
             if (entity == null) throw new ArgumentNullException();
 
             _context.Events.Remove(entity);
+            _logger.Info("Event: " + entity.Id.ToString() + " was removed from database");
         }
 
         public void RemoveRange(IEnumerable<Event> entities)
@@ -73,6 +82,8 @@ namespace Planner.Model.Repositories
             if (!entities.Any()) throw new ArgumentException("No entities on the list");
 
             _context.Events.RemoveRange(entities);
+
+            foreach(var t in entities) _logger.Info("Event: " + t.Id.ToString() + " was removed from database");
         }
 
         public void Update(Event entity)
