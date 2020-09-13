65a9d7bee9f1d91a117e26485e1bc04bc7c7135b
Tomasz Kumiega <tomekkumiega@interia.pl>
2020-09-13 09:24:45 +0200

    Add logger to UnitOfWorkFactory class

--- Planner.Model/Repositories/UnitOfWorkFactory.cs
+++ Planner.Model/Repositories/UnitOfWorkFactory.cs
@@ -4,8 +4,11 @@ namespace Planner.Model.Repositories
 {
     public class UnitOfWorkFactory : IUnitOfWorkFactory
     {
+        private static readonly NLog.Logger _logger = NLog.LogManager.GetCurrentClassLogger();
+
         public IUnitOfWork GetUnitOfWork()
         {
+            _logger.Debug("Unit of work object is created");
             return new UnitOfWork(new ScheduleDbContext(new DbContextOptionsBuilder().UseSqlServer("Server=.;Database=ToDoList;Trusted_Connection=true;").Options));
         }
     }
