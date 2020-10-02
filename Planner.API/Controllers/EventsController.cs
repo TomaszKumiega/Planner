f70d1f887b3e9ce416fa1d7c9f34dd9665f86719
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2020-10-02 11:51:42 +0200

    Add events controller

---
+++ Planner.API/Controllers/EventsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Planner.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EventsController : ControllerBase
+    {
+        private readonly ILogger<EventsController> _logger;
+
+        public EventsController(ILogger<EventsController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
