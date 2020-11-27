31a1d9dc330d84985c5c11ba318b8019ed826783
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2020-11-27 03:55:43 +0100

    Add GetSchedule Async method

--- Planner.Model/Services/ScheduleService.cs
+++ Planner.Model/Services/ScheduleService.cs
@@ -1,13 +1,14 @@
 ﻿using Microsoft.Graph;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Planner.Model.Services
 {
     public class ScheduleService
-    {
-        public List<Event> Events { get; private set; }
+    { 
 
         public Event BuildEvent(string name, int eventType, int eventDifficulty, DateTime startDateTime, DateTime? endDateTime, bool allDay,
             int recurrenceType, int interval, List<Microsoft.Graph.DayOfWeek> daysOfWeek, int index, int month, int? occurrences)
@@ -95,6 +96,75 @@ namespace Planner.Model.Services
                 return new Event(name, (EventType)eventType, (EventDifficulty)eventDifficulty, startDateTime, endDateTime.Value, recurrencePattern, occurrences.Value);
             }
             else return null;
+        }
+
+        private async Task<Dictionary<DateTime, List<Event>>> GetScheduleAsync(DateTime CurrentlyDisplayedDate)
+        {
+            var Schedule = new Dictionary<DateTime, List<Event>>();
+            var days = await Task.Run(() => GetCurrentlyDisplayedDays(CurrentlyDisplayedDate));
+            var events = new List<Event>();
+
+            //Load Events from API
+
+
+            var repetetiveEvents = await Task.Run(() => events.Where(x => x.RecurrencePattern != null).ToList());
+
+            foreach (var t in days)
+            {
+                var listOfEvents = new List<Event>();
+
+                //Finds all repetetive events that are going to happen on the day t
+                foreach (var k in repetetiveEvents)
+                {
+                    if (k.IsDateTimeMatchingRecurrencePattern(t) && k.DaysCompleted.Find(x => x.Date.Equals(t.Date)) == default(DateTime)) listOfEvents.Add(k);
+                }
+
+                // Finds all disposable events that are going to happen on the day t
+                listOfEvents.AddRange(await Task.Run(() => events.Where(x => x.StartDateTime.Value.Date == t.Date && x.RecurrencePattern == null).ToList()));
+
+                Schedule.Add(t, listOfEvents);
+            }
+
+            return Schedule;
+        }
+
+        private List<DateTime> GetCurrentlyDisplayedDays(DateTime CurrentlyDisplayedDate)
+        {
+            var listOfDays = new List<DateTime>();
+            var date = new DateTime(CurrentlyDisplayedDate.Year, CurrentlyDisplayedDate.Month, 1);
+
+            // Generate days from previous months to fill a gap at the begining of the calendar
+            int numberOfDaysFromPreviousMonth;
+
+            // Sunday is 0
+            if ((int)date.DayOfWeek == 0) numberOfDaysFromPreviousMonth = 6;
+            else numberOfDaysFromPreviousMonth = (int)date.DayOfWeek - 1;
+
+            date = date.AddDays(-numberOfDaysFromPreviousMonth);
+
+            for (int i = 0; i < numberOfDaysFromPreviousMonth; i++)
+            {
+                listOfDays.Add(date);
+                date = date.AddDays(1);
+            }
+
+            // Generate list of days from currently displayed month
+            listOfDays.AddRange(Enumerable.Range(1, DateTime.DaysInMonth(date.Year, date.Month))
+                .Select(day => new DateTime(date.Year, date.Month, day))
+                .ToList());
+
+            // Generate days from next month to fill a gap at the end of the calendar
+            date = date.AddMonths(1);
+
+            var numberOfDaysFromNextMonth = 35 - listOfDays.Count;
+
+            for (int i = 0; i < numberOfDaysFromNextMonth; i++)
+            {
+                listOfDays.Add(date);
+                date = date.AddDays(1);
+            }
+
+            return listOfDays;
         }
     }
 }
