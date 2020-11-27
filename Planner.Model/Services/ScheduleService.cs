720284e25e185fd5ea8a471694d4514f74e20b1d
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2020-11-27 03:45:49 +0100

    Add ScheduleService

---
+++ Planner.Model/Services/ScheduleService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Graph;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Planner.Model.Services
+{
+    public class ScheduleService
+    {
+        public List<Event> Events { get; private set; }
+        
+    }
+}
