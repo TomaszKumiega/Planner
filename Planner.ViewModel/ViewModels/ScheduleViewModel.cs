7b31f77090c33c702c7d146661c783bccf134329
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2020-11-27 04:17:11 +0100

    Refactor all methods using ScheduleService

--- Planner.ViewModel/ViewModels/ScheduleViewModel.cs
+++ Planner.ViewModel/ViewModels/ScheduleViewModel.cs
@@ -12,6 +12,7 @@ using DayOfWeek = Microsoft.Graph.DayOfWeek;
 using Event = Planner.Model.Event;
 using EventType = Planner.Model.EventType;
 using User = Planner.Model.User;
+using Planner.Model.Services;
 
 namespace Planner.ViewModel.ViewModels
 {
@@ -35,6 +36,7 @@ namespace Planner.ViewModel.ViewModels
         public PreviousCommand PreviousCommand { get; }
 
         private readonly IUnitOfWorkFactory _unitOfWorkFactory;
+        private readonly IScheduleService _scheduleService;
 
         public DateTime CurrentlyDisplayedDate 
         { 
@@ -48,9 +50,9 @@ namespace Planner.ViewModel.ViewModels
         }
 
 
-        public ScheduleViewModel(IUnitOfWorkFactory unitOfWorkFactory)
+        public ScheduleViewModel(IScheduleService scheduleService)
         {
-            _unitOfWorkFactory = unitOfWorkFactory;
+            _scheduleService = scheduleService;
             DisplayMode = DisplayMode.Month;
             Observers = new List<IObserver>();
             CurrentlySelectedDay = DateTime.Now;
@@ -130,213 +132,27 @@ namespace Planner.ViewModel.ViewModels
 
         public async Task AddEventAsync(string name, int eventType, int eventDifficulty, DateTime startDateTime, DateTime? endDateTime, bool allDay, int recurrenceType, int interval, List<DayOfWeek> daysOfWeek, int index, int month, int? occurrences)
         {
-            RecurrencePattern recurrencePattern = null;
-            Event @event = null;
-
-            switch(recurrenceType)
-            {
-                case -1:
-                    break;
-                case 0:
-                    {
-                        recurrencePattern = new RecurrencePattern();
-                        recurrencePattern.Interval = interval;
-                        recurrencePattern.Type = RecurrencePatternType.Daily;
-                    }
-                    break;
-                case 1:
-                    {
-                        recurrencePattern = new RecurrencePattern();
-                        recurrencePattern.Type = RecurrencePatternType.Weekly;
-                        recurrencePattern.Interval = interval;
-                        recurrencePattern.DaysOfWeek = daysOfWeek;
-                    }
-                    break;
-                case 2:
-                    {
-                        recurrencePattern = new RecurrencePattern();
-                        recurrencePattern.Interval = interval;
-                       
-                        if (index == -1)
-                        {
-                            recurrencePattern.Type = RecurrencePatternType.AbsoluteMonthly;
-                        }
-                        else
-                        {
-                            recurrencePattern.Type = RecurrencePatternType.RelativeMonthly;
-                            recurrencePattern.Index = (WeekIndex)index;
-                            recurrencePattern.DaysOfWeek = daysOfWeek;
-                        }
-                    }
-                    break;
-                case 3:
-                    {
-                        recurrencePattern = new RecurrencePattern();
-                        recurrencePattern.Interval = interval;
-                        
-                        if(index ==-1)
-                        {
-                            recurrencePattern.Type = RecurrencePatternType.AbsoluteYearly;
-                        }
-                        else
-                        {
-                            recurrencePattern.Type = RecurrencePatternType.RelativeYearly;
-                            recurrencePattern.Index = (WeekIndex)index;
-                            recurrencePattern.DaysOfWeek = daysOfWeek;
-                            recurrencePattern.Month = month+1;
-                        }
-                    }
-                    break;
-            }
-
-            if(endDateTime == null && recurrencePattern == null)
-            {
-                @event = new Event(name, (EventType)eventType, (EventDifficulty)eventDifficulty, startDateTime, allDay);
-            }
-            else if(endDateTime.HasValue  && recurrencePattern == null)
-            {
-                @event = new Event(name, (EventType)eventType, (EventDifficulty)eventDifficulty, startDateTime, endDateTime.Value);
-            }
-            else if(endDateTime == null && recurrencePattern != null && occurrences == null)
-            {
-                @event = new Event(name, (EventType)eventType, (EventDifficulty)eventDifficulty, startDateTime, allDay, recurrencePattern);
-            }
-            else if(endDateTime.HasValue && recurrencePattern != null && occurrences == null)
-            {
-                @event = new Event(name, (EventType)eventType, (EventDifficulty)eventDifficulty, startDateTime, endDateTime.Value, recurrencePattern);
-            }
-            else if(endDateTime == null && recurrencePattern != null && occurrences.HasValue)
-            {
-                @event = new Event(name, (EventType)eventType, (EventDifficulty)eventDifficulty, startDateTime, allDay, recurrencePattern, occurrences.Value);
-            }
-            else if(endDateTime.HasValue && recurrencePattern != null && occurrences.HasValue)
-            {
-                @event = new Event(name, (EventType)eventType, (EventDifficulty)eventDifficulty, startDateTime, endDateTime.Value, recurrencePattern, occurrences.Value);
-            }
-
-            using(var unitOfWork = _unitOfWorkFactory.GetUnitOfWork())
-            {
-                unitOfWork.EventRepository.Add(@event);
-                unitOfWork.SaveChanges();
-            }
-
-            await LoadScheduleAsync();
-
+            var @event = await Task.Run(() => _scheduleService.BuildEvent(name, eventType, eventDifficulty, startDateTime, endDateTime, allDay, recurrenceType, interval,
+                daysOfWeek, index, month, occurrences));
+            await _scheduleService.AddEventAsync(@event);
         }
 
         public async Task LoadScheduleAsync()
         {
-            Schedule = new Dictionary<DateTime, List<Event>>();
-
-            var days = await Task.Run(() => GetCurrentlyDisplayedDays());
-
-            using (var unitOfWork = _unitOfWorkFactory.GetUnitOfWork())
-            {
-                var repetetiveEvents = await Task.Run(() => unitOfWork.EventRepository.Find(x => x.RecurrencePattern != null).ToList());
-
-
-                foreach (var t in days)
-                {
-                    var listOfEvents = new List<Event>();
-
-                    //Finds all repetetive events that are going to happen on the day t
-                    foreach (var k in repetetiveEvents)
-                    {
-                        if (k.IsDateTimeMatchingRecurrencePattern(t) && k.DaysCompleted.Find(x => x.Date.Equals(t.Date)) == default(DateTime)) listOfEvents.Add(k);
-                    }
-
-                    // Finds all disposable events that are going to happen on the day t
-                    listOfEvents.AddRange(await Task.Run(() => unitOfWork.EventRepository.Find(x => x.StartDateTime.Value.Date == t.Date && x.RecurrencePattern == null).ToList()));
-
-                    Schedule.Add(t, listOfEvents);
-                }
-            }
+            Schedule = await _scheduleService.GetScheduleAsync(CurrentlyDisplayedDate);
 
             NotifyObservers();
         }
 
-        private List<DateTime> GetCurrentlyDisplayedDays()
-        {
-            var listOfDays = new List<DateTime>();
-            var date = new DateTime(CurrentlyDisplayedDate.Year, CurrentlyDisplayedDate.Month, 1);
-
-            // Generate days from previous months to fill a gap at the begining of the calendar
-            int numberOfDaysFromPreviousMonth;
-
-            // Sunday is 0
-            if ((int)date.DayOfWeek == 0) numberOfDaysFromPreviousMonth = 6;
-            else numberOfDaysFromPreviousMonth = (int)date.DayOfWeek - 1;
-
-            date = date.AddDays(-numberOfDaysFromPreviousMonth);
-
-            for (int i = 0; i < numberOfDaysFromPreviousMonth; i++)
-            {
-                listOfDays.Add(date);
-                date = date.AddDays(1);
-            }
-
-            // Generate list of days from currently displayed month
-            listOfDays.AddRange(Enumerable.Range(1, DateTime.DaysInMonth(date.Year, date.Month))
-                .Select(day => new DateTime(date.Year, date.Month, day))
-                .ToList());
-
-            // Generate days from next month to fill a gap at the end of the calendar
-            date = date.AddMonths(1);
-
-            var numberOfDaysFromNextMonth = 35 - listOfDays.Count;
-
-            for (int i = 0; i < numberOfDaysFromNextMonth; i++)
-            {
-                listOfDays.Add(date);
-                date = date.AddDays(1);
-            }
-
-            return listOfDays;
-        }
-
         public async Task RemoveEventAsync(Event @event)
         {
-            using (var unitOfWork = _unitOfWorkFactory.GetUnitOfWork())
-            {
-                var events = unitOfWork.EventRepository.Find(x => x.Id.Equals(@event.Id)).ToList();
-                if (events.Any())
-                {
-                    var actualEvent = events[0];
-                    unitOfWork.EventRepository.Remove(actualEvent);
-                    unitOfWork.SaveChanges();
-                }
-                else return;
-            }
-
+            await _scheduleService.RemoveEventAsync(@event);
             await LoadScheduleAsync();
         }
 
         public async Task CompleteEventAsync(Event @event, int displayedDay)
         {
-            if (@event == null) throw new ArgumentNullException();
-
-            using (var unitOfWork = _unitOfWorkFactory.GetUnitOfWork())
-            {
-                var events = unitOfWork.EventRepository.Find(x => x.Id == @event.Id).ToList();
-                
-                if (events.Any())
-                {
-                    var actualEvent = events[0];
-
-                    if (actualEvent.RecurrencePattern == null)
-                    {
-                        actualEvent.CompleteEvent(User);
-                        await RemoveEventAsync(actualEvent);
-                    }
-                    else
-                    {
-                        var day = Schedule.ElementAt(displayedDay).Key;
-                        actualEvent.CompleteEvent(User, day);
-                        unitOfWork.SaveChanges();
-                        await LoadScheduleAsync();
-                    }
-                }
-            }
+            await _scheduleService.CompleteEventAsync(@event, displayedDay);
         }
 
         #region IObservable members
