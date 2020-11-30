a3a91587a2c2ae6ed060565facc4847d5973010f
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2020-11-30 02:21:21 +0100

    Add User Service interface

---
+++ Planner.Model/Services/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planner.Model.Services
+{
+    public interface IUserService
+    {
+        User User { get; }
+        string Token { get; }
+
+        Task LoginAsync();
+        Task RegisterAsync();
+        Task UpdateUserAsync();
+    }
+}
