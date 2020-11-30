208386697d6b40fb4a4d299def2dd00f762b339c
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2020-11-30 08:50:58 +0100

    Add automapper profile

---
+++ Planner.API/Helpers/AutoMapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Planner.Model;
+using Planner.Model.Model;
+
+namespace WebApi.Helpers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, UserModel>();
+            CreateMap<RegisterModel, User>();
+            CreateMap<UpdateModel, User>();
+        }
+    }
+}
