ae307961fcecc095123053cc1e57c25d98046844
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2020-11-30 03:11:35 +0100

    Add login method

--- Planner.ViewModel/ViewModels/UserViewModel.cs
+++ Planner.ViewModel/ViewModels/UserViewModel.cs
@@ -1,18 +1,27 @@
-﻿using Microsoft.Graph;
+﻿using Planner.Model;
 using Planner.Model.Services;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Planner.ViewModel.ViewModels
 {
     public class UserViewModel
     {
         private IUserService _userService;
+        public User User { get; private set; }
 
         public UserViewModel(IUserService userService)
         {
             _userService = userService;
         }
+
+        public async Task LoginAsync(string username, string password)
+        {
+            var id = await _userService.LoginAsync(username, password);
+            User = await _userService.GetUserAsync(id);
+        }
+
     }
 }
