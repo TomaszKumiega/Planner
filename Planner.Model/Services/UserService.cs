eaab6b1c9c71d4a6a9cee247fdca8632734691ff
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2020-11-30 02:30:59 +0100

    Implement Login method

--- Planner.Model/Services/UserService.cs
+++ Planner.Model/Services/UserService.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Nancy.Json;
+using Planner.Model.Model;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,17 +15,34 @@ namespace Planner.Model.Services
 
         public string Token { get; private set; }
 
-        public Task LoginAsync(string username, string password)
+        public async Task LoginAsync(string username, string password)
+        {
+            var authModel = new AuthenticateModel(username, password);
+            var authJson = new JavaScriptSerializer().Serialize(authModel);
+
+            var data = new StringContent(authJson, Encoding.UTF8, "application/json");
+
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+
+            using (var client = new HttpClient(clientHandler))
+            {
+                var response = await client.PostAsync(BaseURL + "Users", data);
+
+                string result = response.Content.ReadAsStringAsync().Result;
+
+                Token = result;
+
+                Console.WriteLine(result);
+            }
+        }
+
+        public async Task RegisterAsync(string username, string password, string email, string firstName, string lastName)
         {
             throw new NotImplementedException();
         }
 
-        public Task RegisterAsync(string username, string password, string email, string firstName, string lastName)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task UpdateUserAsync(User user)
+        public async Task UpdateUserAsync(User user)
         {
             throw new NotImplementedException();
         }
