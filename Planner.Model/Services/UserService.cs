c98bde8c4441fe4d267c50241f37c96052bace86
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2020-11-30 02:22:29 +0100

    Add UserService class

---
+++ Planner.Model/Services/UserService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planner.Model.Services
+{
+    public class UserService : IUserService
+    {
+        public User User { get; private set; }
+
+        public string Token { get; private set; }
+
+        public Task LoginAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RegisterAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateUserAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
