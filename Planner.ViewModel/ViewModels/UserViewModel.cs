2a972098176a8ca6b0cd2df6f1d083e132574568
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2020-11-30 03:07:56 +0100

    Add UserViewModel

---
+++ Planner.ViewModel/ViewModels/UserViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Graph;
+using Planner.Model.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Planner.ViewModel.ViewModels
+{
+    public class UserViewModel
+    {
+        private IUserService _userService;
+
+        public UserViewModel(IUserService userService)
+        {
+            _userService = userService;
+        }
+    }
+}
