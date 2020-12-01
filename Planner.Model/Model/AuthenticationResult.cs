785ccc2118febf787e317f48f3ce18a8932be41a
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2020-12-01 05:11:49 +0100

    Change properties order for deserialization

--- Planner.Model/Model/AuthenticationResult.cs
+++ Planner.Model/Model/AuthenticationResult.cs
@@ -11,7 +11,7 @@ namespace Planner.Model.Model
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
-        public string Token { get; set; }
         public int Karma { get; set; }
+        public string Token { get; set; }
     }
 }
